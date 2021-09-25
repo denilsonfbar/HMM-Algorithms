a63f7abf569148872ba19b271d94ab419f2039fc
Denilson Fagundes Barbosa <denilsonfbar@gmail.com>
2021-09-24 23:29:16 -0300

    Forward ok

--- hmm.py
+++ hmm.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math  as mt
 
+
 ## MODEL
 
 n_symbols = 6
@@ -39,61 +40,107 @@ print(transitions_probabilities)
 print(emission_probabilities)
 
 
-## VITERBI DECONDING
+## VITERBI DECODING
 
-# Receives a sequence and presents the best path
-def viterbi(sequence):
+# Receives a sequence and return the most probable path of states
+def viterbi_decoding(sequence):
 
-    n_emissions = sequence.size
-    viterbi_matrix = np.zeros((n_states,n_emissions))
-    paths = np.zeros((n_states,n_emissions))
+    n_positions = sequence.size
+    viterbi_matrix = np.zeros((n_states,n_positions))
+    viterbi_paths = np.zeros((n_states,n_positions))
 
-    for j in range(n_emissions):
+    for i in range(n_positions):
 
-        emitted_symbol = sequence[j]
+        emitted_symbol = sequence[i]
         
-        for i in range(n_states):
+        for l in range(n_states):
     
-            if j == 0:
-                viterbi_matrix[i,j] = initial_probabilities[i] + emission_probabilities[i,emitted_symbol]
+            if i == 0:
+                viterbi_matrix[l,i] = initial_probabilities[l] + emission_probabilities[l,emitted_symbol]
+
             else:
-
                 # Probability of most likely path ending in each state:
-                path_ending_states_probs = np.zeros(n_states)
+                previous_position_probs = np.zeros(n_states)
                 for k in range(n_states):
-                    path_ending_states_probs[k] = viterbi_matrix[k,j-1] + transitions_probabilities[k,i] + emission_probabilities[i,emitted_symbol]
+                    previous_position_probs[k] = viterbi_matrix[k,i-1] + transitions_probabilities[k,l] 
 
-                viterbi_matrix[i,j] = np.amax(path_ending_states_probs)
-                paths[i,j-1] = np.argmax(path_ending_states_probs)
+                viterbi_matrix[l,i] = emission_probabilities[l,emitted_symbol] + np.amax(previous_position_probs) 
+                viterbi_paths[l,i-1] = np.argmax(previous_position_probs)
                 
     # Finishing the paths:
     for k in range(n_states):
-        paths[k,n_emissions-1] = k
+        viterbi_paths[k,n_positions-1] = k
 
+    probability_best_path = np.amax(viterbi_matrix[:,n_positions-1])
+    best_path = viterbi_paths[np.argmax(viterbi_matrix[:,n_positions-1])]
 
     # Checking:
     print("\nViterbi matrix:")
     print (viterbi_matrix)
 
     print("\nPaths: (0: fair; 1: loaded)")
-    print (paths)
+    print (viterbi_paths)
 
-    print("\nBest path:")
-    print(paths[np.argmax(viterbi_matrix[:,n_emissions-1])])
+    print("\nBest path:\tProbability: ", probability_best_path)
+    print(best_path)
+
+    return best_path
 
 
-# Testing:
+## FORWARD
+
+# Receives a sequence and the state of the last position
+# Returns the sum of the probabilities of all paths ending in the state informed
+def forward(sequence, state_last_position):
+
+    n_positions = sequence.size
+    forward_matrix = np.zeros((n_states,n_positions))
+
+    for i in range(n_positions):
+
+        emitted_symbol = sequence[i]
+        
+        for l in range(n_states):
+    
+            if i == 0:
+                forward_matrix[l,i] = initial_probabilities[l] + emission_probabilities[l,emitted_symbol]
+
+            else:
+                # Sum of probability of paths ending in each state:
+                previous_sum_probs = np.zeros(n_states)
+                for k in range(n_states):
+                    previous_sum_probs[k] = forward_matrix[k,i-1] + transitions_probabilities[k,l] 
+
+                forward_matrix[l,i] = emission_probabilities[l,emitted_symbol] + np.sum(previous_sum_probs)
+
+    total_prob = forward_matrix[state_last_position,n_positions-1]
+
+    # Checking:
+    print("\nForward matrix:")
+    print (forward_matrix)
+
+    print("\nSum of probabilities of all paths ending in state ", state_last_position, end=': ')
+    print(total_prob)
+
+    return total_prob
+
+
+## TESTING
+
 def experiment(sequence):
     print("\n---------")
     print("\nSequence:")
     print (sequence)
     for s in range(sequence.size):
         sequence[s] -= 1
-    viterbi(sequence)
+    viterbi_decoding(sequence)
+    forward(sequence, 0)
+    forward(sequence, 1)
 
 sequence = np.array([6,4,1,2])
 experiment(sequence)
 
+'''
 sequence = np.array([6,6,4,1,2])
 experiment(sequence)
 
@@ -102,3 +149,4 @@ experiment(sequence)
 
 sequence = np.array([1,2,3,4,6,6,6,1,2,3])
 experiment(sequence)
+'''

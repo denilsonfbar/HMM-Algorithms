e2dbfc7bb29e6386215af7021a598eab17788ef4
Denilson Fagundes Barbosa <denilsonfbar@gmail.com>
2021-09-29 23:55:59 -0300

    Backward ok

--- hmm.py
+++ hmm.py
@@ -1,5 +1,6 @@
 import numpy as np
-import math  as mt
+
+verbose = True
 
 model = 2
 
@@ -48,22 +49,9 @@ elif model == 2:
                                        [0.1,0.9]])
 
 ## Converting the probabilities to log in base 2:
-for i in range(n_states):
-    initial_probabilities[i] = mt.log(initial_probabilities[i], 2) 
-for i in range(n_states):
-    for j in range(n_states):
-        transitions_probabilities[i,j] = mt.log(transitions_probabilities[i,j], 2) 
-for i in range(n_states):
-    for j in range(n_symbols):
-        emission_probabilities[i,j] = mt.log(emission_probabilities[i,j], 2) 
-# Checking:
-print("\nProbabilities in log base 2 values:")
-print("Initial probabilities:")
-print(initial_probabilities)
-print("Transitions probabilities:")
-print(transitions_probabilities)
-print("Emission probabilities:")
-print(emission_probabilities)
+initial_probabilities = np.log2(initial_probabilities)
+transitions_probabilities = np.log2(transitions_probabilities)
+emission_probabilities = np.log2(emission_probabilities)
 
 
 ## VITERBI DECODING
@@ -85,7 +73,7 @@ def viterbi_decoding(sequence):
                 viterbi_matrix[l,i] = initial_probabilities[l] + emission_probabilities[l,emitted_symbol]
 
             else:
-                # Probability of most likely path ending in each state:
+                # Probability of most likely path starting in each state:
                 previous_position_probs = np.zeros(n_states)
                 for k in range(n_states):
                     previous_position_probs[k] = viterbi_matrix[k,i-1] + transitions_probabilities[k,l] 
@@ -100,23 +88,26 @@ def viterbi_decoding(sequence):
     probability_best_path = np.amax(viterbi_matrix[:,n_positions-1])
     best_path = viterbi_paths[np.argmax(viterbi_matrix[:,n_positions-1])]
 
-    # Checking:
-    print("\nViterbi matrix:")
-    print (viterbi_matrix)
-
-    print("\nPaths: ")
-    print (viterbi_paths)
-
-    print("\nBest path:\tProbability: ", probability_best_path)
-    print(best_path)
+    if verbose:
+        print("\nViterbi matrix:")
+        print (viterbi_matrix)
+        print("\nPaths: ")
+        print (viterbi_paths)
+        print("\nBest path:\tProbability: ", probability_best_path)
+        print(best_path)
 
     return best_path
+
+
+# Global variables for reuse of matrices
+forward_matrix = []
+backward_matrix = []
 
 
 ## FORWARD
 
 # Receives a sequence and the state of the last position
-# Returns the sum of the probabilities of all paths ending in the state of the last position informed
+# Returns the sum of the probabilities of all paths finishing in the state of the last position informed
 def forward(sequence, state_last_position):
 
     n_positions = sequence.size
@@ -144,18 +135,60 @@ def forward(sequence, state_last_position):
 
     total_prob = forward_matrix[state_last_position,n_positions-1]
 
-    # Checking:
-    print("\nForward matrix with log base 2 values:")
-    print (forward_matrix)
+    if verbose:
+        print("\nForward matrix with log base 2 values:")
+        print (forward_matrix)
+        print("\nForward matrix with real values:")
+        print (np.exp2(forward_matrix))
+        print("\nSum of probabilities of all paths finishing in state ", state_last_position, end=': ')
+        print(total_prob)
 
-    for i in range(n_positions):
+    return total_prob
+
+
+## BACKWARD
+
+# Receives a sequence and the state of the first position
+# Returns the sum of the probabilities of all paths starting in the state of the first position informed
+def backward(sequence, state_first_position):
+
+    n_positions = sequence.size
+    backward_matrix = np.zeros((n_states,n_positions+1))
+
+    for i in reversed(range(n_positions)):
+
+        emitted_symbol = sequence[i]
+
+        # Initialization:
+        if i == n_positions-1:
+            for l in range(n_states):
+                backward_matrix[l,n_positions] = np.log2(1)
+        
         for l in range(n_states):
-            forward_matrix[l,i] = 2 ** forward_matrix[l,i]
-    print("\nForward matrix with real values:")
-    print (forward_matrix)
+            # Induction:
+            if i != 0:
+                for k in range(n_states):
+                    aux = backward_matrix[k,i+1] + transitions_probabilities[l,k] + emission_probabilities[k,emitted_symbol]
 
-    print("\nSum of probabilities of all paths ending in state ", state_last_position, end=': ')
-    print(total_prob)
+                    if k == 0:
+                        later_sum_probs = aux
+                    else:
+                        later_sum_probs = np.logaddexp2(later_sum_probs, aux) 
+
+                backward_matrix[l,i] = later_sum_probs
+            # Termination:
+            else:
+                backward_matrix[l,i] = backward_matrix[l,i+1] + emission_probabilities[l,emitted_symbol] + initial_probabilities[l]
+
+    total_prob = backward_matrix[state_first_position,0]
+
+    if verbose:
+        print("\nBackward matrix with log base 2 values:")
+        print (backward_matrix)
+        print("\nBackward matrix with real values:")
+        print (np.exp2(backward_matrix))
+        print("\nSum of probabilities of all paths starting in state ", state_first_position, end=': ')
+        print(total_prob)
 
     return total_prob
 
@@ -171,6 +204,8 @@ def experiment(sequence):
             sequence[s] -= 1
     viterbi_decoding(sequence)
     forward(sequence, 0)
+    backward(sequence, 0)
+
 
 if model == 1:
 
